5f5bfe7428d436d8e1cbf62481cdf5642e6e8bf7
Mertnode <mertnode@gmail.com>
2024-03-27 12:58:31 +0300

    .

---
+++ api/models/userSchema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "Please add a full name"]
+    },
+    email: {
+        type: String,
+        required: [true,"Please add an email"],
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true,"Please add a password"],
+        minLength: [6,"Password must be at least 6 characters"]
+    },
+    image: {
+        type: String
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+  likedMovies: [
+      {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Movie"
+      }
+  ]
+},{timestamps:true})
